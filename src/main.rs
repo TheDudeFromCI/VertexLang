6d93f2023f9b59837938ca814d8c3352c04e7ca6
TheDudeFromCI <thedudefromci@gmail.com>
2021-12-14 04:59:14 -0700

    Removed unused utils mod
    Signed-off-by: TheDudeFromCI <thedudefromci@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 mod compiler;
 mod core;
-mod utils;
 
 fn main() {
     println!("Hello, world!");
--- src/utils.rs
+++
@@ -1,9 +0,0 @@
-pub trait StringUtils {
-    fn substring(&self, start: usize, len: usize) -> Self;
-}
-
-impl StringUtils for String {
-    fn substring(&self, start: usize, len: usize) -> Self {
-        self.chars().skip(start).take(len).collect()
-    }
-}
